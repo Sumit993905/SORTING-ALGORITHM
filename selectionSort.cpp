23d3ec6c8b73eeec6a57dfe3ad3261ee2d1782fd
Sumit <officialsumitraj993905@gmail.com>
2024-04-22 01:12:08 +0530

    part1

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
 using namespace std ;
 
+// only for printing an array 
 void printSortedArray(int arr[], int n){
     for(int i=0; i<n; i++){
         cout<<arr[i]<<" ";
     }
 }
 
+// sorting Algorithm
 void selectionSort(int arr[], int n){
 
     for(int i=0; i<n-1; i++){
@@ -27,9 +29,9 @@ int main(){
 
     int arr[5]={12,17,8,4,25};
 
-    selectionSort(arr,5);
+    selectionSort(arr,5);  //calling Sorting Algorithm 
     
-    printSortedArray(arr,5);
+    printSortedArray(arr,5); // Here calling for Printing an Sorted Array
 
     return 0;
 }
